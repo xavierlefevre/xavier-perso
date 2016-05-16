87965d039df5911fbfada3e2dc1654e4090a004a
Xavier Lefèvre <xavier.lefevre@me.com>
2016-05-16 22:52:26 +0200

    refining the link list

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -5,14 +5,14 @@ export default class Lists extends Component {
     return (
       <div className="ui list">
         {
-          this.props.items.map((x, i) =>
-            (<div className="item" key={i}>
-              <i className="book icon"></i>
+          this.props.items.map((v, i) =>
+            (<a href={v.link} target="_blank" className="item" key={i}>
+              <i className={`big black bordered ${v.icon} icon`}></i>
               <div className="content">
-                <div className="header">{x.title}</div>
-                {x.description}
+                <div className="header">{v.title}</div>
+                {v.description}
               </div>
-            </div>)
+            </a>)
           )
         }
       </div>
--- src/components/Themes.js
+++ src/components/Themes.js
@@ -9,68 +9,94 @@ export default class Themes extends Component {
         {
           title: 'Professional',
           description: 'Related to work',
-          icon: 'suitcase icon',
+          icon: 'suitcase',
           items: [
             {
               title: 'Github',
               description: 'Repo Website',
+              icon: 'github',
+              link: 'https://github.com/xavierlefevre',
             },
             {
               title: 'LinkedIn',
               description: 'Social Pro Website',
+              icon: 'linkedin',
+              link: 'https://fr.linkedin.com/in/lefevrexavier',
             },
             {
               title: 'HopWork',
               description: 'Freelance Platform',
+              icon: 'plus',
+              link: 'https://www.hopwork.fr/profile/xavierlefevre',
             },
           ],
         },
         {
           title: 'Education',
           description: 'My different learning platforms',
-          icon: 'book icon',
+          icon: 'book',
           items: [
             {
               title: 'Coursera',
               description: 'Silicon Valley Learning Site',
+              icon: 'plus',
+              link: 'https://www.coursera.org/user/i/e06bd90f072ab3d5503bb97d83b44592',
+            },
+            {
+              title: 'Duolingo',
+              description: 'Digital Courses in French',
+              icon: 'plus',
+              link: 'https://www.duolingo.com/xavierlefevre',
             },
             {
               title: 'Khan Academy',
               description: 'Basic Courses',
+              icon: 'plus',
+              link: 'https://www.khanacademy.org/profile/xavierlefevre/',
             },
             {
               title: 'Open Classroom',
               description: 'Digital Courses in French',
+              icon: 'plus',
+              link: 'https://openclassrooms.com/membres/xavierlefevre-1',
             },
           ],
         },
         {
           title: 'Private',
           description: 'Mainly social personal networks',
-          icon: 'spy icon',
+          icon: 'spy',
           items: [
             {
               title: 'Facebook',
               description: 'Need to present?',
+              icon: 'facebook',
+              link: 'https://www.facebook.com/xavier.lefevre',
             },
             {
               title: 'Twitter',
               description: 'Not very active',
+              icon: 'twitter',
+              link: 'https://twitter.com/xavier_lef',
             },
           ],
         },
         {
           title: 'Design/Video',
           description: 'My attemps to design and video edition',
-          icon: 'paint brush icon',
+          icon: 'paint brush',
           items: [
             {
               title: 'YouTube',
               description: 'My perso videos',
+              icon: 'youtube play',
+              link: 'https://www.youtube.com/c/xavierlefevrefr',
             },
             {
-              title: 'Dribble',
+              title: 'Dribbble',
               description: 'Not yet live',
+              icon: 'dribbble',
+              link: '',
             },
           ],
         },
@@ -84,8 +110,10 @@ export default class Themes extends Component {
         {
           this.state.themes.map((v, i) =>
             (<div className="four wide column" key={i}>
-              <h2 className="ui medium header">
-                <i className={v.icon}></i>
+              <h2 className="ui centered medium header">
+                <div>
+                  <i className={`huge ${v.icon} icon`}></i>
+                </div>
                 <div className="content">
                   {v.title}
                   <div className="sub header">{v.description}</div>
